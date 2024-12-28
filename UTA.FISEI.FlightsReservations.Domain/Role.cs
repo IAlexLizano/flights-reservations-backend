f67ff9ae4f74221efc083622098d63a00135abc4
Alex <alexsanty10lm@gmail.com>
2024-12-27 20:43:34 -0500

    Creación de las entidades relacionadas a usuarios en domain

---
+++ UTA.FISEI.FlightsReservations.Domain/Role.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTA.FISEI.FlightsReservations.Domain
+{
+    [DataContract]
+    public class Role
+    {
+        [DataMember]
+        public int roleId { get; set; }
+        [DataMember]
+        public string role { get; set; }
+    }
+
+}
